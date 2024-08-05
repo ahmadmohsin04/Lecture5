e14d5057378179393c7567a4b23223887d9fb1ea
Ahmad Mohsin <business.ahmadmohsin@gmail.com>
2024-08-05 12:51:06 +0500

    two recursive problems

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
 public class Main {
-    static void print (){
-        System.out.println("New Message");
-    }
+//    static void print (){
+//        System.out.println("New Message");
+//    }
+//
+//    static void print1 (){
+//        System.out.println("New Message");
+//    }
 
-    static void print1 (){
-        System.out.println("New Message");
-    }
+//    static void printSeries (int num){
+//        if (num > 1) {
+//            System.out.println(num);
+//            printSeries(num-1);
+//        }
+//    }
+
+//    public static int countDigit (int num){
+//        if (num/10 == 0)
+//            return 1;
+//        return 1 + countDigit(num/10);
+//    }
     public static void main(String[] args) {
-        System.out.println("Vamos");
-        print();
+//        System.out.println(countDigit(123));
+//    printSeries(4);
     }
 }
 
