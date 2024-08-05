b5d235b7bf0470e9f2db4e454788f54f16198842
Ahmad Mohsin <business.ahmadmohsin@gmail.com>
2024-08-05 11:48:08 +0500

    second

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
-
+    static void print (){
+        System.out.println("New Message");
+    }
     public static void main(String[] args) {
         System.out.println("Vamos");
+        print();
     }
 }
