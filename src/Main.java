0dcb5c766ff6ad4329f767029bfc5edf746a5ed2
Ahmad Mohsin <business.ahmadmohsin@gmail.com>
2024-08-05 13:10:44 +0500

    more recursive problems

--- src/Main.java
+++ src/Main.java
@@ -19,9 +19,26 @@ public class Main {
 //            return 1;
 //        return 1 + countDigit(num/10);
 //    }
+
+//    private static int fact (int num){
+//        if (num==0 || num==1){
+//            return num;
+//        }
+//        int prevfact = fact(num-1);
+//        return num*prevfact;
+//    }
+
+    private static int power (int num , int p){
+        if (p==0)
+            return 1;
+        int prevpow = power(num , p-1);
+        return prevpow*num;
+    }
+
     public static void main(String[] args) {
 //        System.out.println(countDigit(123));
 //    printSeries(4);
+//        System.out.println(fact(4));
     }
 }
 
