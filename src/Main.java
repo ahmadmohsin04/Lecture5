2a9bd5fa350c87594c921dce5988a81c14728584
Ahmad Mohsin <business.ahmadmohsin@gmail.com>
2024-08-05 12:07:12 +0500

    all functions done

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ public class Main {
     static void print (){
         System.out.println("New Message");
     }
+
+    static void print1 (){
+        System.out.println("New Message");
+    }
     public static void main(String[] args) {
         System.out.println("Vamos");
         print();
     }
 }
+
